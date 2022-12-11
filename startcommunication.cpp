c4141d292c649083050935ada1a167a983eaed26
m-RNA <m-RNA@qq.com>
2022-12-12 06:50:07 +0800

    复用之前写的代码，实现串口收发，先完成再完美

--- startcommunication.cpp
+++ startcommunication.cpp
@@ -18,6 +18,9 @@ StartCommunication::StartCommunication(QWidget *parent) :
 
     connect(this, &StartCommunication::serialStateChange, this, &StartCommunication::uiLookUpdata);
 
+    // 链接串口接收到信息信号
+    connect(serial, &QSerialPort::readyRead, this, &StartCommunication::serialReadyRead_Slot);
+
 
 }
 
@@ -40,7 +43,7 @@ void StartCommunication::setDeviceName(QString s)
     ui->gbxSerialSetting->setTitle(deviceName);
 }
 
-// 更新界面串口信息
+// 扫描更新界面串口信息
 void StartCommunication::serialInfoUpdata(void)
 {
     QStringList serialNamePort;
@@ -65,6 +68,8 @@ StartCommunication::~StartCommunication()
     }
     delete ui;
 }
+
+// 串口开关 初始化
 void StartCommunication::on_btnSerialSwitch_clicked()
 {
     qDebug() << deviceName << "点击串口开关";
@@ -121,12 +126,12 @@ void StartCommunication::on_btnSerialSwitch_clicked()
             qDebug() << deviceName << "串口打开失败！";
             serialState = false; // 串口状态 置关
 
-            QMessageBox::critical(this, "标准仪器串口打开失败!",
-                                  "请检查:\n\n\
-线缆是否松动?\n\
-串口号是否正确?\n\
-串口是否被序占用?\n\
-是否有串口读写权限?");
+            QMessageBox::critical(this, deviceName,"串口打开失败!\n"
+"请检查:\n\
+- 线缆是否松动?\n\
+- 串口号是否正确?\n\
+- 串口是否被序占用?\n\
+- 是否有串口读写权限?");
         }
     }
     else
@@ -161,8 +166,82 @@ void StartCommunication::uiLookUpdata(bool state)
     }
 }
 
-/*  串口接收任务   */
+void StartCommunication::on_btnClearRecvTE_clicked()
+{
+    ui->teRecv->clear();
+}
 
+void StartCommunication::on_btnClearSendTE_clicked()
+{
+    ui->teSend->clear();
+}
 
+/*   串口接包任务   */
+void StartCommunication::serialReadyRead_Slot()
+{
+    QByteArray realtimeRxBuf; // 最新接收数据
+    realtimeRxBuf = serial->readAll();
 
+    // 暂停接收时，读完串口消息就退出不处理
+    if(recvPauseState == true) return;
 
+    QString qsBuf = QString(realtimeRxBuf);
+
+    ui->teRecv->insertPlainText(qsBuf);       // 将消息附到recvTextEdit(连续)
+    ui->teRecv->moveCursor(QTextCursor::End); // 滑动条保持在最低部
+
+    QByteArray rxFrame;
+    static QByteArray staticTemp; // 静态中间变量
+    int startIndex = -1;
+
+    staticTemp.append(realtimeRxBuf); // 读取串口，附在 staticTemp 之后
+    startIndex = staticTemp.lastIndexOf("\n"); //获取"\n"的下标
+
+    if (startIndex >= 0)
+    {
+        rxFrame.append(staticTemp.left(startIndex -1)); //带"\n"一起复制进去
+        // this->vSerial->vSerialData->rxByteCnt += rxFrame.length(); // 更新rx计数值
+        staticTemp.remove(0, startIndex + 1); // 移除"\n"之前的内容
+    }
+
+    if (rxFrame.isEmpty())
+        return;
+
+    qDebug() << "RxFrame:" << rxFrame;
+    // 解析数据
+    // {text}23.3
+    // printf("filtemp=%f\r\n",rtd);
+    int title_index_left;
+    title_index_left = rxFrame.indexOf("}");
+    realtimeRxBuf = rxFrame.replace("{", "").left(title_index_left - 1);
+    qDebug() << "Title:" << QString(realtimeRxBuf);
+    int titleLength = realtimeRxBuf.length();
+    double data;
+    data = QString(rxFrame.right(rxFrame.length() - 1- titleLength)).toDouble();
+    qDebug() << "Temp:" << data;
+
+//    ui->customPlot->graph(0)->addData(m_xLength, data);// 添加数据
+//    ui->customPlot->graph(0)->setName("标准 "+QString("%1℃").arg(data));// 设置图例名称
+
+//    // 曲线能动起来的关键在这里，设定x轴范围为最近80个数据
+//    ui->customPlot->xAxis->setRange(m_xLength, 80, Qt::AlignRight); // 右对齐
+//    // 刷新画图
+//    ui->customPlot->replot();
+
+//    // 自动缩放（要画完才调用）
+//    ui->customPlot->graph(0)->rescaleAxes(); // 只会缩小 不会放大
+//    m_xLength++;
+}
+
+/*   串口接收任务   */
+void StartCommunication::on_btnSend_clicked()
+{
+    serial->write(ui->teSend->toPlainText().toLocal8Bit().data());
+}
+
+void StartCommunication::on_cbPause_toggled(bool checked)
+{
+    qDebug() << deviceName << "点击暂停combox" << checked;
+    recvPauseState = checked;
+    emit serialPauseStateChange(recvPauseState);
+}
--- startcommunication.h
+++ startcommunication.h
@@ -27,15 +27,27 @@ public slots:
 private slots:
     void on_btnSerialSwitch_clicked();
     void uiLookUpdata(bool state);
+    void serialReadyRead_Slot();
+
+    void on_btnClearRecvTE_clicked();
+
+    void on_btnSend_clicked();
+
+    void on_btnClearSendTE_clicked();
+
+    void on_cbPause_toggled(bool checked);
 
 signals:
     void serialStateChange(bool state);
+    void serialPauseStateChange(bool state);
+    
 
 private:
     Ui::StartCommunication *ui;
     QString deviceName = "未知仪器"; // 需要初始化变量，不然会程序会异常退出 参考B站：BV1U14y1K7Po
     QSerialPort *serial;
-    bool serialState = false; // 串口状态 true为开 false为关
+    bool serialState = false;    // 串口状态 true为开 false为关
+    bool recvPauseState = false; // 串口暂停接收状态 true为暂停 false为正常
 
     void serialInfoUpdata(void);
 
