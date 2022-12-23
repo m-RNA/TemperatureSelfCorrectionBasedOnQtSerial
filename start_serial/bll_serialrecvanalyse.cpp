d7d89ed01456cf1cc4ddf9c9e2458c64dcef1ac9
m-RNA <m-RNA@qq.com>
2022-12-23 23:18:52 +0800

    修复多个回车换行无法正确解析数据的错误

--- start_serial/bll_serialrecvanalyse.cpp
+++ start_serial/bll_serialrecvanalyse.cpp
@@ -16,38 +16,39 @@ Bll_SerialRecvAnalyse::~Bll_SerialRecvAnalyse()
 // void Bll_SerialRecvAnalyse::run()
 void Bll_SerialRecvAnalyse::working(QByteArray rxRowData)
 {
-    // qDebug() << "分析线程ID：" << QThread::currentThread();
+    // qDebug() << "rxRowData:" << QString(rxRowData);
 
     QByteArray rxFrame;
-    int startIndex = -1;
+    int endIndex;
 
     staticTemp.append(rxRowData); // 读取串口，附在 staticTemp 之后
-
-    // BUG显然 待优化？
-    startIndex = staticTemp.lastIndexOf("\n"); // 获取"\n"的索引
-
-    // \r\n 和 \n 两种情况 待优化
-    if (startIndex >= 0)
+    while (1)
     {
-        rxFrame.append(staticTemp.left(startIndex)); // 去除\n"
-        staticTemp.remove(0, startIndex + 1);        // 移除"\n"与"\n"之前的内容
+        endIndex = staticTemp.indexOf("\n"); // 获取"\n"的索引
+
+        // \r\n 和 \n 两种情况
+        if (endIndex >= 0)
+        {
+            if (staticTemp.at(endIndex - 1) == '\r')
+                rxFrame.append(staticTemp.left(endIndex - 1)); // 去除\n"
+            else
+                rxFrame.append(staticTemp.left(endIndex)); // 去除\n"
+            staticTemp.remove(0, endIndex + 1);            // 移除"\n"与"\n"之前的内容
+        }
+
+        if (rxFrame.isEmpty())
+            return;
+
+        int title_index_left;
+        title_index_left = rxFrame.indexOf("}");
+        rxRowData = rxFrame.replace("{", "").left(title_index_left - 1);
+        // qDebug() << "Title:" << QString(rxRowData);
+        int titleLength = rxRowData.length();
+        double data;
+        data = QString(rxFrame.right(rxFrame.length() - 1 - titleLength)).toDouble();
+        qDebug() << "解析" << data;
+
+        emit sgBll_AnalyseFinish(data);
+        rxFrame.clear();
     }
-
-    if (rxFrame.isEmpty())
-        return;
-
-    // qDebug() << "RxFrame:" << rxFrame;
-    // 解析数据
-    // {text}23.3
-    // printf("temp=%f\r\n",rtd);
-    int title_index_left;
-    title_index_left = rxFrame.indexOf("}");
-    rxRowData = rxFrame.replace("{", "").left(title_index_left - 1);
-    // qDebug() << "Title:" << QString(rxRowData);
-    int titleLength = rxRowData.length();
-    double data;
-    data = QString(rxFrame.right(rxFrame.length() - 1 - titleLength)).toDouble();
-    qDebug() << "解析" << data;
-
-    emit sgBll_AnalyseFinish(data);
 }
